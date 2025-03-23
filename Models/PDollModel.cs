7b6046cdb00fc0775a42c9a15fcb880a044aecfd
bdcoller <bdcoller@gmail.com>
2025-03-23 18:22:35 -0500

    initial commit (not much there) for PlayterDoll scene and model.

---
+++ Models/PDollModel.cs
@@ -0,0 +1,19 @@
+//============================================================================
+// PDollModel.cs
+// Model of the Playter Doll.
+//============================================================================
+using Godot;
+using System;
+
+public partial class PDollModel : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ Worlds/PlayterDoll.cs
@@ -0,0 +1,19 @@
+//============================================================================
+// PlayterDoll.cs
+// Code for runing the Godot scene of rotating playter doll
+//============================================================================
+using Godot;
+using System;
+
+public partial class PlayterDoll : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
