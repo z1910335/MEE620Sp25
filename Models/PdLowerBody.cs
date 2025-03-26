0f606f32626963eb3957dc28f170b5efbdc93e93
bdcoller <bdcoller@gmail.com>
2025-03-25 22:33:54 -0500

    init commit PdLowerBody.cs

---
+++ Models/PdLowerBody.cs
@@ -0,0 +1,23 @@
+//============================================================================
+// PDLowerBody.cs
+// Immediate Mesh for the lower body.
+//============================================================================
+using Godot;
+using System;
+
+public partial class PdLowerBody : MeshInstance3D
+{
+
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	// public override void _Process(double delta)
+	// {
+	// }
+}
