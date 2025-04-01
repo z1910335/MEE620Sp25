5e1794ff94b2b479c0de472822239329ff440dfa
bdcoller <bdcoller@gmail.com>
2025-03-31 21:09:44 -0500

    initial commit

---
+++ Student/PlayterStudent.cs
@@ -0,0 +1,22 @@
+//============================================================================
+// PlayterSim.cs
+// Simulation of a Playter Doll.
+//
+// This is where students write their code.
+//============================================================================
+using System;
+
+public partial class PlayterSim : Simulator
+{
+    
+
+    //------------------------------------------------------------------------
+    // RHSFuncPlayter:  Evaluates the right sides of the differential
+    //                   equations for the Playter Doll
+    //------------------------------------------------------------------------
+    private void RHSFuncPlayter(double[] xx, double t, double[] ff)
+    {
+
+    }
+
+} // end class
