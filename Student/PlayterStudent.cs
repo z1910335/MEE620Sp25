ae2d6ac5673a89255e21f4a98d090e830228996e
bdcoller <bdcoller@gmail.com>
2025-04-05 19:17:30 -0500

    zero out the RHS of odes by default

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -34,6 +34,16 @@ public partial class PlayterSim : Simulator
         xG      = xx[14];
         yG      = xx[15];
         zG      = xx[16];
+
+
+
+        // COMMENT THESE OUT OR REMOVE WHEN READY
+        ff[0] = ff[1] = ff[2] = 0.0;   // derivs of body angular velocities set to zero
+        ff[3] = ff[4] = 0.0;           // derivs of arm angular velocities
+        ff[5] = ff[6] = ff[7] = 0.0;   // derivs of cener of mass velocities
+        ff[8] = ff[9] = ff[10] = ff[11] = 0.0;  // derivs of quaternion coords
+        ff[12] = ff[13] = 0.0;         // derivs of arm angles
+        ff[14] = ff[15] = ff[16] = 0.0;  // derivs of CG coordinates
     }
 
 } // end class
